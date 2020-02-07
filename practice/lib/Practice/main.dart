8719832c80780edef93e40f8304030834613ce27
rfperfect895@gmail.com <u6368195@anu.edu.au>
2020-02-07 17:47:43 +0800

    message

--- practice/lib/Practice/main.dart
+++ practice/lib/Practice/main.dart
@@ -1,4 +1,5 @@
- import 'package:flutter/material.dart';
+
+import 'package:flutter/material.dart';
 import 'package:practice/Practice/classTime.dart';
 import 'package:practice/Practice/courses.dart';
 import 'package:practice/Practice/overview.dart';
