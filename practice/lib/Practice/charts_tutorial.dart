7e56f946ae8e14c671c2e6fc50b63b99bd51ac26
rfperfect895@gmail.com <u6368195@anu.edu.au>
2020-02-07 17:45:40 +0800

    Changes successfully added

---
+++ practice/lib/Practice/charts_tutorial.dart
@@ -0,0 +1,11 @@
+import 'package:charts_flutter/flutter.dart';
+import 'package:flutter/material.dart';
+
+
+class ClicksPerYear{
+  final String year;
+  final int clicks;
+
+  ClicksPerYear(this.year, this.clicks);
+
+}
